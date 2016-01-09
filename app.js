616c9051659814394fbacd812df988db322c25f2
AndreLuizHaag <andreluizhaag@gmail.com>
2016-01-09 14:01:06 -0200

    Inclusão de novas rotas

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+/**
+ * Rest full API - Skeleton
+ *
+ * API esqueleto para a criação de novas API's.
+ *
+ * @author André Luiz Haag <andreluizhaag@gmail.com>
+ * @license LICENSE.md
+ */
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
@@ -8,7 +16,7 @@ var jwt = require('jwt-simple');
 var router = express.Router();
 
 var config = require('./config/config');
-var routes = require('./routes/index');
+var test = require('./routes/index');
 var user = require('./routes/user');
 var news = require('./routes/news');
 var login = require('./routes/login');
@@ -17,21 +25,27 @@ var accessValidation = require('./middlewares/access-validation');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  return next();
+});
 app.set('secret', config.secret); // secret variable
 app.use(logger('dev')); // use morgan to log requests to the console
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/doc', express.static(__dirname + '/doc')); // prove a doc estaticamente
+app.use(cookieParser());
 mongoose.connect(config.database);
 
 // API ROUTES -------------------
-app.use('/', routes);
+app.use('/', test);
 app.use('/login', login);
 app.use('/logout', accessValidation, logout);
 app.use('/user', accessValidation, user);
-app.use('/news', accessValidation, news);
+app.use('/news', /*accessValidation,*/ news);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
